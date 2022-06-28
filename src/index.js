2b03562fdf7d923dea0cd9ae6666d8a0a960e273
Denys <denkach2211@gmail.com>
2022-06-28 20:34:02 +0200

    solve issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import fetchCoutries from './fetchCountries.js';
-import notiflix, { Notify } from 'Notiflix'
+import { Notify } from 'Notiflix'
 
 
 const DEBOUNCE_DELAY = 300;
